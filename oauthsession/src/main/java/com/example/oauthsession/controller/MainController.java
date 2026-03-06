8c2bf2332179688f5c90713c1780816623a11b94
hjh <ktorn21@naver.com>
2026-03-06 18:36:31 +0900

    초기세팅

---
+++ oauthsession/src/main/java/com/example/oauthsession/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.example.oauthsession.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MainController {
+    
+    @GetMapping("/")
+    public String mainPage() {
+
+        return "main";
+    }
+    
+
+}
